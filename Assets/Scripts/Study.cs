7496055f14f51b1667913deeab3b56eb252dd5cb
Valentin <valentin.kao@epitech.eu>
2018-03-15 18:13:07 +0800

    Start

---
+++ Assets/Scripts/Study.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Study : MonoBehaviour
+{
+    [SerializeField]
+    private string name = "Study";
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    [SerializeField]
+    private bool recording = true;
+
+    [SerializeField]
+    private bool replayLastRecord = false;
+   
+}
